efc17db4a049804189eac338fb5abf12d4ca25ed
amaan <amaanurrahman00@gmail.com>
2023-03-05 14:50:03 +0530

    updated

--- src/main/java/com/example/emosic/EmosicBackendApplication.java
+++ src/main/java/com/example/emosic/EmosicBackendApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmosicBackendApplication {
 
 	public static void main(String[] args) {
-		//can remove
-		String MONGO_USER = System.getenv("MONGO_USER");
-		String MONGO_PASSWORD = System.getenv("MONGO_PASSWORD");
-		String MONGO_CLUSTER = System.getenv("MONGO_CLUSTER");
 		SpringApplication.run(EmosicBackendApplication.class, args);
 	}
 
